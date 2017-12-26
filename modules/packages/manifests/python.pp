dd299b25ddb5e9dae72eded0108d4493d20c90af
Anthony Sottile <asottile@umich.edu>
2017-12-25 21:18:34 -0800

    Fix ppa

--- modules/packages/manifests/python.pp
+++ modules/packages/manifests/python.pp
@@ -1,7 +1,7 @@
 class packages::python {
   package { ['python-dev', 'python3-dev']: ensure => 'latest' }
 
-  apt::ppa { 'ppa:deadsnakes': } ->
+  apt::ppa { 'ppa:deadsnakes/ppa': } ->
   package { ['python2.6-dev', 'python3.4-dev', 'python3.6-dev']:
     ensure => 'latest',
   }
