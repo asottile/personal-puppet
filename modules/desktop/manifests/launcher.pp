8c281fd2828bb623afa05b2d9fe20c65343077b9
Anthony Sottile <asottile@umich.edu>
2017-12-30 11:50:54 -0800

    Add pidgin to the launcher

--- modules/desktop/manifests/launcher.pp
+++ modules/desktop/manifests/launcher.pp
@@ -1,13 +1,15 @@
 class desktop::launcher {
   gsetting { 'com.canonical.Unity.Launcher favorites':
-    ensure => [
+    ensure  => [
       'application://org.gnome.Nautilus.desktop',
       'application://gnome-terminal.desktop',
       'application://firefox.desktop',
+      'application://pidgin.desktop',
       'application://unity-control-center.desktop',
       # built in unity things
       'unity://running-apps', 'unity://expo-icon', 'unity://devices',
     ],
-    user   => 'asottile',
+    user    => 'asottile',
+    require => [Package['pidgin']],
   }
 }
