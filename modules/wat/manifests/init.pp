55032e88d0f445117138f8f84b3eb3d410886e60
Anthony Sottile <asottile@umich.edu>
2018-01-09 16:37:02 -0800

    Remove wat file

--- modules/wat/manifests/init.pp
+++ modules/wat/manifests/init.pp
@@ -1,9 +1,3 @@
 class wat {
-  file {
-    strip('/home/asottile/wat '):
-      ensure => file,
-      source => 'puppet:///modules/wat/wat',
-      owner  => 'asottile',
-      group  => 'asottile',
-  }
+  file { '/home/asottile/wat': ensure => 'absent' }
 }
