bf27a8bd0f6a0f484256dae0831fceec7863829f
Anthony Sottile <asottile@umich.edu>
2018-06-21 12:54:39 -0700

    Also set the lock background

--- modules/desktop/manifests/background.pp
+++ modules/desktop/manifests/background.pp
@@ -1,11 +1,13 @@
 class desktop::background {
-  $schema = 'org.gnome.desktop.background'
-  $key = 'picture-uri'
   $bg = 'file:///usr/share/backgrounds/Twilight_Frost_by_Phil_Jackson.jpg'
 
   package { 'ubuntu-wallpapers-precise': ensure => 'present' } ->
   gsetting { 'org.gnome.desktop.background picture-uri':
-    ensure => 'file:///usr/share/backgrounds/Twilight_Frost_by_Phil_Jackson.jpg',
+    ensure => $bg,
+    user   => 'asottile',
+  } ->
+  gsetting { 'org.gnome.desktop.screensaver picture-uri':
+    ensure => $bg,
     user   => 'asottile',
   }
 }
