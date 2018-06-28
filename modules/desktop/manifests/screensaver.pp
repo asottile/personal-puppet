cea236107571a082def0d9d1b04b22af09c20e6c
Anthony Sottile <asottile@umich.edu>
2018-06-27 22:21:03 -0700

    Turns out this isn't necessary

--- modules/desktop/manifests/screensaver.pp
+++ modules/desktop/manifests/screensaver.pp
@@ -1,8 +1,4 @@
 class desktop::screensaver {
-  gsetting { 'org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type':
-    ensure => 'nothing',
-    user   => 'asottile',
-  }
   gsetting { 'org.gnome.desktop.session idle-delay':
     ensure => 0,
     user   => 'asottile',
