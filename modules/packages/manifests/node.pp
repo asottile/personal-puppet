9c862ecdfcc6a682e9807570601184efe70dc91e
Anthony Sottile <asottile@umich.edu>
2018-10-01 15:51:46 -0700

    Install nodejs from nodesource instead of bionic :'(

--- modules/packages/manifests/node.pp
+++ modules/packages/manifests/node.pp
@@ -1,3 +1,16 @@
 class packages::node {
-  package { ['nodejs', 'npm']: ensure => 'latest' }
+  # TODO: remove after all have updated
+  package { ['npm']: ensure => 'absent' } ->
+  apt::key { '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280':
+    source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
+  } ->
+  apt::source { 'nodesource':
+    location => 'https://deb.nodesource.com/node_10.x',
+    repos    => 'main',
+    release  => $::lsbdistcodename,
+  } ->
+  package { 'nodejs':
+    ensure  => 'latest',
+    require => Exec['apt_update'],
+  }
 }
