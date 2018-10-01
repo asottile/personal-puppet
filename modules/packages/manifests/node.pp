b950df365548207f10cbd2a27eb976930dc400b4
Anthony Sottile <asottile@umich.edu>
2018-10-01 16:07:15 -0700

    Unnecessary list literal

--- modules/packages/manifests/node.pp
+++ modules/packages/manifests/node.pp
@@ -1,6 +1,6 @@
 class packages::node {
   # TODO: remove after all have updated
-  package { ['npm']: ensure => 'absent' } ->
+  package { 'npm': ensure => 'absent' } ->
   apt::key { '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280':
     source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
   } ->
