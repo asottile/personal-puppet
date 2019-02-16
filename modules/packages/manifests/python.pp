447b3b738448d87de78fd58ac41aa65e1ee31aa3
Anthony Sottile <asottile@umich.edu>
2019-02-16 12:28:45 -0800

    Add python3.8

--- modules/packages/manifests/python.pp
+++ modules/packages/manifests/python.pp
@@ -1,8 +1,11 @@
 class packages::python {
   package { ['python-dev', 'python3-dev']: ensure => 'latest' }
 
+  $deadsnakes_pkgs = [
+    'python3.5-dev', 'python3.7-dev', 'python3.8-dev', 'python3.8-distutils',
+  ]
   apt::ppa { 'ppa:deadsnakes/ppa': } ->
-  package { ['python3.5-dev', 'python3.7-dev']:
+  package { $deadsnakes_pkgs:
     ensure  => 'latest',
     require => Exec['apt_update'],
   }
