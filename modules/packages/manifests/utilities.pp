707cfab863f7788b5d0127632b2fb25d1f76dc4f
Anthony Sottile <asottile@umich.edu>
2019-06-10 08:12:56 -0700

    Install tmux

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,6 +1,7 @@
 class packages::utilities {
   $tools = [
-    'curl', 'dos2unix', 'graphviz', 'inotify-tools', 'jq', 'sqlite3', 'tree',
+    'curl', 'dos2unix', 'graphviz', 'inotify-tools', 'jq', 'sqlite3', 'tmux',
+    'tree',
   ]
   package { $tools: ensure => 'latest' }
 }
