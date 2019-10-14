80c517b657855affad99bedc231ad59db62ca9da
Anthony Sottile <asottile@umich.edu>
2019-10-14 16:48:58 -0700

    Upgrade pypy

--- modules/desktop/manifests/pypy.pp
+++ modules/desktop/manifests/pypy.pp
@@ -1,10 +1,10 @@
 class desktop::pypy {
   # https://pypy.org/download.html
 
-  $pypy2 = 'pypy2.7-v7.1.1-linux64'
-  $pypy2_sha256 = '73b09ef0860eb9ad7997af3030b22909806a273d90786d78420926df53279d66'
-  $pypy3 = 'pypy3.6-v7.1.1-linux64'
-  $pypy3_sha256 = '8014f63b1a34b155548852c7bf73aab2d41ebddf2c8fb603dc9dd8509be93db0'
+  $pypy2 = 'pypy2.7-v7.2.0-linux64'
+  $pypy2_sha256 = '05acf28e6a243026ecad933b9361d8f74b41f00818071b76b38c4694cc4c9599'
+  $pypy3 = 'pypy3.6-v7.2.0-linux64'
+  $pypy3_sha256 = 'aa128e555ad0fe5c4c15104ae0903052bd232b6e3a73f5fe023d27b8fd0d6089'
 
   archive { "/tmp/${pypy2}.tar.bz2":
     ensure        => 'present',
@@ -58,6 +58,8 @@ class desktop::pypy {
   file { [
     '/home/asottile/opt/pypy2-v6.0.0-linux64',
     '/home/asottile/opt/pypy3-v6.0.0-linux64',
+    '/home/asottile/opt/pypy2.7-v7.1.1-linux64',
+    '/home/asottile/opt/pypy3.6-v7.1.1-linux64',
   ]:
     ensure  => 'absent',
     recurse => true,
