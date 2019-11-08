4106f31e85097b2fd87a44130d2b9f7a18bf2407
Anthony Sottile <asottile@umich.edu>
2019-11-08 10:51:20 -0800

    ubuntu late-shipped a python3.8 package

--- modules/packages/manifests/python.pp
+++ modules/packages/manifests/python.pp
@@ -1,14 +1,11 @@
 class packages::python {
-  package { ['python-dev', 'python3-dev']: ensure => 'latest' }
+  $ubuntu_pkgs = ['python-dev', 'python3-dev', 'python3-distutils']
+  package { $ubuntu_pkgs: ensure => 'latest' }
 
-  $deadsnakes_pkgs = [
-    'python3.5-dev', 'python3.7-dev', 'python3.8-dev', 'python3.8-distutils',
-  ]
+  $deadsnakes_pkgs = ['python3.5-dev', 'python3.7-dev', 'python3.8-dev']
   apt::ppa { 'ppa:deadsnakes/ppa': } ->
   package { $deadsnakes_pkgs:
     ensure  => 'latest',
     require => Exec['apt_update'],
   }
-  # TODO: remove once all have updated
-  package { 'python2.6-dev': ensure => 'purged' }
 }
