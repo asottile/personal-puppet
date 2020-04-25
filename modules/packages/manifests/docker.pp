dc8ccf3efb584e7b1e991af6beb1bf2548530837
Anthony Sottile <asottile@umich.edu>
2020-04-25 14:20:09 -0700

    Use eoan codename to install docker on focal

--- modules/packages/manifests/docker.pp
+++ modules/packages/manifests/docker.pp
@@ -1,4 +1,11 @@
 class packages::docker {
+  # TODO: replace with focal once updated
+  if $::lsbdistcodename == 'focal' {
+    $docker_dist_codename = 'eoan'
+  } else {
+    $docker_dist_codename = $::lsbdistcodename
+  }
+
   apt::key { '9DC858229FC7DD38854AE2D88D81803C0EBFCD88':
     source => 'https://download.docker.com/linux/ubuntu/gpg',
   } ->
@@ -6,7 +13,7 @@ class packages::docker {
     architecture => $::architecture,
     location     => 'https://download.docker.com/linux/ubuntu',
     repos        => 'stable',
-    release      => $::lsbdistcodename,
+    release      => $docker_dist_codename,
   } ->
   package { 'docker-ce':
     ensure  => 'latest',
