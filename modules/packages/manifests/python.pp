3437940d54f26093c6eb65931819292dbc3a9ce2
Anthony Sottile <asottile@umich.edu>
2020-04-25 14:27:23 -0700

    Update python for focal

--- modules/packages/manifests/python.pp
+++ modules/packages/manifests/python.pp
@@ -1,23 +1,19 @@
 class packages::python {
-  $ubuntu_pkgs = ['python-dev', 'python3-dev', 'python3-distutils']
+  $ubuntu_pkgs = ['python2-dev', 'python3-dev', 'python3-distutils']
   package { $ubuntu_pkgs: ensure => 'latest' } ->
-  file { '/etc/python3.6/sitecustomize.py':
+  file { '/etc/python3.8/sitecustomize.py':
     ensure  => present,
     content => '',
   }
 
-  $deadsnakes_pkgs = ['python3.7-dev', 'python3.8-dev', 'python3.9-dev']
+  $deadsnakes_pkgs = [
+    'python3.6-dev',
+    'python3.7-dev', 'python3.7-distutils',
+    'python3.9-dev', 'python3.9-distutils',
+  ]
   apt::ppa { 'ppa:deadsnakes/ppa': } ->
   package { $deadsnakes_pkgs:
     ensure  => 'latest',
     require => Exec['apt_update'],
-  }
-
-  ['python3.7', 'python3.8', 'python3.9'].each |String $python| {
-    file {"/etc/${python}/sitecustomize.py":
-      ensure  => present,
-      content => '',
-      require => Package["${python}-dev"],
-    }
   }
 }
