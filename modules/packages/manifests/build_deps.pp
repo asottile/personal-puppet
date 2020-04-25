162e21293419a8a1209052e4babae9c704692894
Anthony Sottile <asottile@umich.edu>
2020-04-25 16:52:14 -0700

    Add build deps for tmux

--- modules/packages/manifests/build_deps.pp
+++ modules/packages/manifests/build_deps.pp
@@ -3,15 +3,15 @@ class packages::build_deps {
     # git
     'libcurl4-openssl-dev', 'libssl-dev', 'zlib1g-dev',
     # cpython
-    'libffi-dev',
-    'libreadline-dev',
-    'libsqlite3-dev',
+    'libffi-dev', 'libreadline-dev', 'libsqlite3-dev',
     # pyyaml
     'libyaml-dev',
     # pypy
     'pkg-config',
     # scss_lint
     'ruby-dev',
+    # tmux
+    'autoconf', 'bison', 'libevent-dev',
   ]
   package { $deps: ensure => 'latest' }
 }
