4c4a9fc105bcb73ccf0a8e851cdd54e55b195cc7
Anthony Sottile <asottile@umich.edu>
2020-04-25 16:37:16 -0700

    Install mlocate to provide locate

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,7 +1,7 @@
 class packages::utilities {
   $tools = [
-    'curl', 'dos2unix', 'graphviz', 'inotify-tools', 'jq', 'net-tools',
-    'rlwrap', 'sqlite3', 'tmux', 'tree',
+    'curl', 'dos2unix', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
+    'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree',
   ]
   package { $tools: ensure => 'latest' }
 }
