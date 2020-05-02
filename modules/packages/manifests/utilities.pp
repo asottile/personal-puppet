e7f0be934fa8b040db19caab18b7284029d645e8
Anthony Sottile <asottile@umich.edu>
2020-05-01 17:29:43 -0700

    add ffmpeg

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,6 +1,6 @@
 class packages::utilities {
   $tools = [
-    'curl', 'dos2unix', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
+    'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
     'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree',
   ]
   package { $tools: ensure => 'latest' }
