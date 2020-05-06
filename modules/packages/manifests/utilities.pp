f2415d8846c763c7451092132662edd9f71dc945
Anthony Sottile <asottile@umich.edu>
2020-05-06 11:31:30 -0700

    add neofetch

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,7 +1,7 @@
 class packages::utilities {
   $tools = [
     'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
-    'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree', 'xclip',
+    'neofetch', 'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree', 'xclip',
   ]
   package { $tools: ensure => 'latest' }
 }
