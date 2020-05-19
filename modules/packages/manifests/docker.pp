6ed1241e1e73bed5fbf538251b9a70235f4e2b15
Anthony Sottile <asottile@umich.edu>
2020-05-19 11:08:46 -0700

    Revert "Use eoan codename to install docker on focal"
    This reverts commit dc8ccf3efb584e7b1e991af6beb1bf2548530837.

--- modules/packages/manifests/docker.pp
+++ modules/packages/manifests/docker.pp
@@ -1,11 +1,4 @@
 class packages::docker {
-  # TODO: replace with focal once updated
-  if $::lsbdistcodename == 'focal' {
-    $docker_dist_codename = 'eoan'
-  } else {
-    $docker_dist_codename = $::lsbdistcodename
-  }
-
   apt::key { '9DC858229FC7DD38854AE2D88D81803C0EBFCD88':
     source => 'https://download.docker.com/linux/ubuntu/gpg',
   } ->
@@ -13,7 +6,7 @@ class packages::docker {
     architecture => $::architecture,
     location     => 'https://download.docker.com/linux/ubuntu',
     repos        => 'stable',
-    release      => $docker_dist_codename,
+    release      => $::lsbdistcodename,
   } ->
   package { 'docker-ce':
     ensure  => 'latest',
