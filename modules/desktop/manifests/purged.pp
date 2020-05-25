a32f03dfb421a59e674787c6c4cf75801008b40b
Anthony Sottile <asottile@umich.edu>
2020-05-25 12:40:42 -0700

    make sure gitk is purged

--- modules/desktop/manifests/purged.pp
+++ modules/desktop/manifests/purged.pp
@@ -1,4 +1,4 @@
 class desktop::purged {
-  $purged = ['libreoffice-common', 'printer-driver-foo2zjs', 'snapd']
+  $purged = ['gitk', 'libreoffice-common', 'printer-driver-foo2zjs', 'snapd']
   package {$purged: ensure => purged}
 }
