6f0ed2f67227d7659cd570f1b48e66fd2211a33f
Anthony Sottile <asottile@umich.edu>
2020-08-02 14:50:55 -0700

    purge command-not-found as well

--- modules/desktop/manifests/purged.pp
+++ modules/desktop/manifests/purged.pp
@@ -1,4 +1,7 @@
 class desktop::purged {
-  $purged = ['gitk', 'libreoffice-common', 'printer-driver-foo2zjs', 'snapd']
-  package {$purged: ensure => purged}
+  $purged = [
+    'command-not-found', 'gitk', 'libreoffice-common',
+    'printer-driver-foo2zjs', 'snapd',
+  ]
+  package {$purged: ensure => 'purged'}
 }
