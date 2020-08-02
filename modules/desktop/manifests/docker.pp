18c1db4e367958146bb847096c7b3c99cb8e0aff
Anthony Sottile <asottile@umich.edu>
2020-08-02 15:04:27 -0700

    replace docker with podman

--- modules/desktop/manifests/docker.pp
+++ modules/desktop/manifests/docker.pp
@@ -1,13 +1,21 @@
 class desktop::docker {
   include packages::docker
 
-  exec { 'add asottile to docker group':
-    command => 'usermod --append --groups docker asottile',
-    unless  => join([
+  exec { 'remove asottile from docker group':
+    command => 'gpasswd --delete asottile docker',
+    onlyif  => join([
         'id --name --groups --zero asottile | ',
         'grep --quiet --null-data --line-regexp docker',
     ]),
     path    => '/usr/sbin:/usr/bin:/bin',
     require => Package['docker-ce'],
+  }
+
+  file { '/home/asottile/bin/docker':
+    ensure  => 'link',
+    target  => '/usr/bin/podman',
+    owner   => 'asottile',
+    group   => 'asottile',
+    require => [Package['podman'], File['/home/asottile/bin']],
   }
 }
--- modules/packages/manifests/docker.pp
+++ modules/packages/manifests/docker.pp
@@ -1,15 +1,17 @@
 class packages::docker {
   apt::key { '9DC858229FC7DD38854AE2D88D81803C0EBFCD88':
+    ensure => 'absent',
     source => 'https://download.docker.com/linux/ubuntu/gpg',
   } ->
   apt::source { 'docker-ce':
+    ensure       => 'absent',
     architecture => $::architecture,
     location     => 'https://download.docker.com/linux/ubuntu',
     repos        => 'stable',
     release      => $::lsbdistcodename,
   } ->
   package { 'docker-ce':
-    ensure  => 'latest',
+    ensure  => 'purged',
     require => Exec['apt_update'],
   }
 }
