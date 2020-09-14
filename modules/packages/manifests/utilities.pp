05f7cec7227bda3e417e082afb45bab80928a64f
Anthony Sottile <asottile@umich.edu>
2020-09-14 12:23:10 -0700

    add ncdu

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,7 +1,8 @@
 class packages::utilities {
   $tools = [
     'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
-    'neofetch', 'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree', 'xclip',
+    'ncdu', 'neofetch', 'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree',
+    'xclip',
   ]
   package { $tools: ensure => 'latest' }
 }
