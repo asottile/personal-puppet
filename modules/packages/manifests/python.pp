a0fce31229865522efd68ca6f2334144c79b6164
Anthony Sottile <asottile@umich.edu>
2020-10-05 20:27:48 -0700

    add python3.10

--- modules/packages/manifests/python.pp
+++ modules/packages/manifests/python.pp
@@ -10,6 +10,7 @@ class packages::python {
     'python3.6-dev',
     'python3.7-dev', 'python3.7-distutils',
     'python3.9-dev', 'python3.9-distutils',
+    'python3.10-dev', 'python3.10-distutils',
   ]
   apt::ppa { 'ppa:deadsnakes/ppa': } ->
   package { $deadsnakes_pkgs:
