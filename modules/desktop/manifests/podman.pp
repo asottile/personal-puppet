36cbfdab6764c32ed06511842f285f4ff964660b
Anthony Sottile <asottile@umich.edu>
2022-04-07 20:43:43 -0400

    use native overlayfs for podman

--- modules/desktop/manifests/podman.pp
+++ modules/desktop/manifests/podman.pp
@@ -8,15 +8,8 @@ class desktop::podman {
   }
 
   file { '/home/asottile/.config/containers':
-    ensure => 'directory',
-    owner  => 'asottile',
-    group  => 'asottile',
-  } ->
-  file { '/home/asottile/.config/containers/storage.conf':
-    ensure  => 'file',
-    source  => 'puppet:///modules/desktop/containers/storage.conf',
-    owner   => 'asottile',
-    group   => 'asottile',
-    require => Package['fuse-overlayfs'],
+    ensure  => 'absent',
+    recurse => true,
+    force   => true,
   }
 }
