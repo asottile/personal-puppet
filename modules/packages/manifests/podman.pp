76c90bb37422ad1f86f3ee8649686e8251e16b79
Anthony Sottile <asottile@umich.edu>
2023-08-29 20:35:24 -0400

    use unstable podman again

--- modules/packages/manifests/podman.pp
+++ modules/packages/manifests/podman.pp
@@ -1,3 +1,11 @@
 class packages::podman {
-  package { 'podman': ensure => 'latest' }
+  apt::key { '2472D6D0D2F66AF87ABA8DA34D64390375060AA4':
+    source => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${::lsbdistrelease}/Release.key",
+  } ->
+  apt::source { 'podman':
+    location => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${::lsbdistrelease}/",
+    release  => '',
+    repos    => '/',
+  } ->
+  package { ['buildah', 'podman', 'fuse-overlayfs'] : ensure => 'latest' }
 }
