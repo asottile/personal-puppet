c06a1045f64bedb2ccf6ffc509a1b01927555ab4
Anthony Sottile <asottile@umich.edu>
2024-05-04 13:24:06 -0400

    updates for noble

--- modules/packages/manifests/podman.pp
+++ modules/packages/manifests/podman.pp
@@ -1,11 +1,3 @@
 class packages::podman {
-  apt::key { '2472D6D0D2F66AF87ABA8DA34D64390375060AA4':
-    source => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${facts['os']['distro']['release']['full']}/Release.key",
-  } ->
-  apt::source { 'podman':
-    location => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${facts['os']['distro']['release']['full']}/",
-    release  => '',
-    repos    => '/',
-  } ->
   package { ['buildah', 'podman', 'fuse-overlayfs'] : ensure => 'latest' }
 }
