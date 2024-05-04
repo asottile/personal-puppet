98b01d03c7a32947ba3d150a80d588e93a9d61b3
Anthony Sottile <asottile@umich.edu>
2024-05-04 13:23:42 -0400

    mlocate -> plocate

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -1,7 +1,7 @@
 class packages::utilities {
   $tools = [
-    'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
-    'ncdu', 'neofetch', 'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree',
+    'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'ncdu',
+    'neofetch', 'net-tools', 'plocate', 'rlwrap', 'sqlite3', 'tmux', 'tree',
     'xclip',
   ]
   package { $tools: ensure => 'latest' }
