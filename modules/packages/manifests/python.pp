a4dc09449985922f0d0fca1c3cc73dc2e1e34f21
anthony sottile <asottile@umich.edu>
2025-06-20 15:07:00 -0400

    install python3.14

--- modules/packages/manifests/python.pp
+++ modules/packages/manifests/python.pp
@@ -12,6 +12,7 @@ class packages::python {
     'python3.10-dev', 'python3.10-distutils',
     'python3.11-dev',
     'python3.13-dev',
+    'python3.14-dev',
   ]
   apt::ppa { 'ppa:deadsnakes/ppa': } ->
   package { $deadsnakes_pkgs:
