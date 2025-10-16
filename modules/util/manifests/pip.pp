45211270a3ed1235dba663bfcde13e7539cfa1dc
Anthony Sottile <asottile@umich.edu>
2025-10-16 12:05:12 -0400

    fix util::pip for packages which differ in -/_ such as pre-commit

--- modules/util/manifests/pip.pp
+++ modules/util/manifests/pip.pp
@@ -1,7 +1,8 @@
 define util::pip(String $pkg, String $venv) {
+  $pkg_re = regsubst($pkg, '-', '[-_]')
   exec { "pip install ${pkg} into ${venv}":
     command => "${venv}/bin/pip install ${pkg}",
-    unless  => "${venv}/bin/pip freeze | grep '^${pkg}=='",
+    unless  => "${venv}/bin/pip freeze | grep '^${pkg_re}=='",
     user    => 'asottile',
   }
 }
